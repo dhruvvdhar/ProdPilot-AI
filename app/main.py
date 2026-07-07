a07e740ba1a2cfbf00a6381cb4b5be7871ea191f
DHRUV DHAR <ds7435@srmist.edu.in>
2026-07-07 19:11:15 +0530

    Add cors in main.py

--- app/main.py
+++ app/main.py
@@ -1,20 +1,24 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.core.observability import enable_tracing
-from fastapi import FastAPI
+
 from app.api.v1.auth import router as auth_router
 from app.api.v1.chat import router as chat_router
 from app.api.v1.documents import router as documents_router
 from app.api.v1.rag import router as rag_router
-from fastapi import HTTPException
 from app.api.v1 import documents
+from app.api.v1.conversations import (
+    router as conversations_router,
+)
+
 from app.core.exceptions import ProdPilotException
 from app.core.exception_handlers import (
     prodpilot_exception_handler,
     generic_exception_handler,
     http_exception_handler,
 )
-from app.api.v1.conversations import (
-    router as conversations_router,
-)
+
 from app.services.hybrid_search.bm25_service import (
     bm25_service,
 )
@@ -25,8 +29,19 @@ tracer.__enter__()
 app = FastAPI(
     title="ProdPilot AI",
     version="2.0.0",
-    description="AI Powered Production Support Platform"
+    description="AI Powered Production Support Platform",
 )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 bm25_service.load_index()
 
 app.add_exception_handler(
@@ -51,10 +66,11 @@ app.include_router(rag_router)
 app.include_router(documents.router)
 app.include_router(conversations_router)
 
+
 @app.get("/")
 def root():
     return {
         "application": "ProdPilot AI",
         "version": "2.0.0",
-        "status": "Running"
+        "status": "Running",
     }
